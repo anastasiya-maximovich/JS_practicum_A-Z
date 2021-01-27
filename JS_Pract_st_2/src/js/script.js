5fec18ca009f5f736ba8bc3bff42430a9bd9e76d
anastasiya023 <anastasiya025.am@gmail.com>
2021-01-27 16:51:18 +0300

    feat: create JS_Pract_st_2

--- JS_Pract_st_2/src/js/script.js
+++ JS_Pract_st_2/src/js/script.js
@@ -1,35 +1,89 @@
 'use strict';
 
-const adv = document.querySelectorAll('.promo__adv img');
-const bg = document.querySelector('.promo__bg');
-const genre = bg.querySelector('.promo__genre');
-const movieList = document.querySelector('.promo__interactive-list');
+document.addEventListener('DOMContentLoaded', () =>{
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+          bg = document.querySelector('.promo__bg'),
+          genre = bg.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
 
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-adv.forEach(item => {
-    item.remove();
-});
+    addForm.addEventListener('submit', (event) =>{
+        event.preventDefault();
 
-genre.textContent = 'драма';
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
 
-bg.style.backgroundImage = 'url(img/bg.png)';
+        if(newFilm){  // тут проверка на true, т.е. если ВВЕДЕНО
 
-movieList.innerHTML = '';
+            if(newFilm.length > 21){
+                newFilm = `${newFilm.slice(0, 21)}...`;
+            }
 
-movieDB.movies.sort().forEach((item, i) => { //тут сразу сортировка. можно вынести в однельную строку movieDB.movies.sort();
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i+1}. ${item}
-            <div class="delete"></div>
-        </li>
-    `;
+            if(favorite){ // тут проверка на true ,т.к. если НАЖАТА 
+                console.log('Добавлен любимый фильм');
+            }
+            movieDB.movies.push(newFilm);
+            sortArray(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+    });
+
+    const deleteArr = (arr) =>{
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+    
+    const createStyle = () => {
+        genre.textContent = 'драма';
+
+        bg.style.backgroundImage = 'url(img/bg.png)';
+    };
+   
+    const sortArray = (arr) =>{
+        arr.sort();
+    };
+
+
+    const createMovieList = (film, perent) =>{
+        perent.innerHTML = '';
+        sortArray(film);  //мы хотим, чтобы при вызове функции еще и шла сортировка
+
+        film.forEach((item, i) => { 
+            perent.innerHTML += `
+                <li class="promo__interactive-item">${i+1}. ${item}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });                 //далее логика такова: после создания листа фильмов,мы можем какой-то элемент и удалить
+        document.querySelectorAll('.delete').forEach((btn, i) => {  //т.к. нам больше "корзинки" не надо, обращаемся к ним без создания переменной
+            btn.addEventListener('click', () =>{
+                btn.parentElement.remove();             //обращаемся к родителю "корзинки" и удаляем его
+                movieDB.movies.splice(i, 1);            //удаляем элемент и из списка :  splice вырезает кусок (от, сколько резать)
+                createMovieList(film, perent);          //после удаления элемента, происходит рекурсия (функция вызывает сама себя) и перезаписывает список
+            });
+        });
+    };
+
+   
+    deleteArr(adv);
+    createStyle();
+    createMovieList(movieDB.movies, movieList);
+
 });
