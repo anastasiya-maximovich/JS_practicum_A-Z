cab6176e568764bca2173a6694d640db10268034
Anastasiya Maximovich <63050839+anastasiya-maximovich@users.noreply.github.com>
2021-02-02 21:52:16 +0300

    feat: add simpleAnimation
    Add  folder 'simpleAnimation'

---
+++ sympleAnimation/main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('button');
+let timerId,
+    i = 0;
+
+function myAmime (){
+   const box = document.querySelector('.box');
+   
+   let pos = 0;
+
+   const id = setInterval(move, 10);
+   function move(){
+       if(pos === 300){
+           clearInterval();
+       }else{
+           pos++;
+           box.style.top = pos + 'px';
+           box.style.left = pos + 'px';
+       }
+   }
+}
+
+btn.addEventListener('click', myAmime);
