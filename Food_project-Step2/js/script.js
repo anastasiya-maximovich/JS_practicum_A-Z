883a368d846567439ae0d72cbdf7d64b4172ec8b
Anastasiya Maximovich <63050839+anastasiya-maximovich@users.noreply.github.com>
2021-02-06 20:53:14 +0300

    refactor: remove some comments
    remove some comments

--- Food_project-Step2/js/script.js
+++ Food_project-Step2/js/script.js
@@ -17,11 +17,11 @@ window.addEventListener('DOMContentLoaded', ()=>{
             item.classList.remove('show', 'fade');
         });
         tabs.forEach(item =>{
-            item.classList.remove('tabheader__item_active'); // кроме скрыть все табы, удаляем Класс Активности у Каждого таба
+            item.classList.remove('tabheader__item_active');
         });
     }
 
-    function showTabContent(i = 0){   // передаем акгумент по умолчанию, если в вызове функции мы не передаем аргументы
+    function showTabContent(i = 0){   
         tabContent[i].classList.add('show', 'fade');
         tabContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
@@ -33,9 +33,9 @@ window.addEventListener('DOMContentLoaded', ()=>{
     tabHeader.addEventListener('click', (event) =>{
         const target = event.target;
 
-        if(target && target.classList.contains('tabheader__item')){ //пользователь кликнул именно в Таб
+        if(target && target.classList.contains('tabheader__item')){ 
             tabs.forEach((item, i) => {
-                if(target == item){                  // проверяем, чтобы элемент в Табе такой же, в который кликнул пользователь
+                if(target == item){
                     hideTabContent();
                     showTabContent(i);
                 }
